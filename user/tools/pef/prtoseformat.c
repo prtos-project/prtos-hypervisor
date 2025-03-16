f6ac93ac647241f60f32718a1cd6a5ba55954b69
18939117 <18939117@qq.com>
2025-03-16 20:54:27 +0800

    [ARMv8] Enable host assist tool `pef` to support ARMv8
    Signed-off-by: 18939117 <18939117@qq.com>

--- user/tools/pef/prtoseformat.c
+++ user/tools/pef/prtoseformat.c
@@ -63,6 +63,12 @@ void error_printf(char *fmt, ...) {
 #define EM_ARCH EM_386
 #endif
 
+#if defined(CONFIG_AARCH64)
+#define EM_ARCH EM_AARCH64  // 更准确的 64位 ARM 架构标识符
+#define ELF(x) Elf64_##x
+#define PRINT_PREF
+#endif
+
 static int parse_elf_image(int fd_elf) {
     struct prtos_image_hdr *prtos_image_hdr = 0;
     struct pef_segment *pef_sect;
@@ -83,6 +89,8 @@ static int parse_elf_image(int fd_elf) {
 
     lseek(fd_elf, 0, SEEK_SET);
     DO_READ(fd_elf, &e_hdr, sizeof(ELF(Ehdr)));
+
+
     if ((RHALF(e_hdr.e_type) != ET_EXEC) || (e_hdr.e_machine != RHALF(EM_ARCH)) || (e_hdr.e_phentsize != RHALF(sizeof(ELF(Phdr)))))
         error_printf("Malformed ELF header");
 
@@ -101,7 +109,7 @@ static int parse_elf_image(int fd_elf) {
             break;
         }
 #else
-    Elf32_Shdr SHSTRTAB;
+    ELF(Shdr) SHSTRTAB;
     lseek(fd_elf, 0, SEEK_SET);
     lseek(fd_elf, e_hdr.e_shoff + (e_hdr.e_shentsize * e_hdr.e_shstrndx), SEEK_SET);
     DO_READ(fd_elf, &SHSTRTAB, e_hdr.e_shentsize);
@@ -149,6 +157,7 @@ static int parse_elf_image(int fd_elf) {
             }
         }
     }
+
     if (!prtos_hdr && !prtos_image_hdr) error_printf("Neither .prtos_hdr nor .prtos_image_hdr found");
 
     DO_MALLOC(pef_custom_file_table, sizeof(struct pef_custom_file) * pef_hdr.num_of_custom_files);
@@ -433,20 +442,20 @@ static void print_header(void) {
         else
             fprintf(stderr, "%02x ", pef_hdr_read->payload[e]);
     fprintf(stderr, "\n");
-    fprintf(stderr, TAB "file size: %" PRINT_PREF "u\n", RWORD(pef_hdr_read->file_size));
-    fprintf(stderr, TAB "segment table offset: %" PRINT_PREF "u\n", RWORD(pef_hdr_read->segment_table_offset));
+    fprintf(stderr, TAB "file size: %" PRINT_PREF "llu\n", RWORD(pef_hdr_read->file_size));
+    fprintf(stderr, TAB "segment table offset: %" PRINT_PREF "llu\n", RWORD(pef_hdr_read->segment_table_offset));
     fprintf(stderr, TAB "no. segments: %d\n", RWORD(pef_hdr_read->num_of_segments));
-    fprintf(stderr, TAB "custom_file table offset: %" PRINT_PREF "u\n", RWORD(pef_hdr_read->custom_file_table_offset));
+    fprintf(stderr, TAB "custom_file table offset: %" PRINT_PREF "llu\n", RWORD(pef_hdr_read->custom_file_table_offset));
     fprintf(stderr, TAB "no. custom_files: %d\n", RWORD(pef_hdr_read->num_of_custom_files));
-    fprintf(stderr, TAB "image offset: %" PRINT_PREF "d\n", RWORD(pef_hdr_read->image_offset));
-    fprintf(stderr, TAB "image length: %" PRINT_PREF "d\n", RWORD(pef_hdr_read->image_length));
+    fprintf(stderr, TAB "image offset: %" PRINT_PREF "lld\n", RWORD(pef_hdr_read->image_offset));
+    fprintf(stderr, TAB "image length: %" PRINT_PREF "lld\n", RWORD(pef_hdr_read->image_length));
     if (RWORD(pef_hdr_read->flags) & PEF_TYPE_PARTITION)
-        fprintf(stderr, TAB "page table: [0x%" PRINT_PREF "x - 0x%" PRINT_PREF "x]\n", RWORD(pef_hdr_read->page_table),
+        fprintf(stderr, TAB "page table: [0x%" PRINT_PREF "llx - 0x%" PRINT_PREF "llx]\n", RWORD(pef_hdr_read->page_table),
                 RWORD(pef_hdr_read->page_table) + RWORD(pef_hdr_read->page_table_size));
-    fprintf(stderr, TAB "prtos image's header: 0x%" PRINT_PREF "x\n", RWORD(pef_hdr_read->prtos_image_hdr));
-    fprintf(stderr, TAB "entry point: 0x%" PRINT_PREF "x\n", RWORD(pef_hdr_read->entry_point));
+    fprintf(stderr, TAB "prtos image's header: 0x%" PRINT_PREF "llx\n", RWORD(pef_hdr_read->prtos_image_hdr));
+    fprintf(stderr, TAB "entry point: 0x%" PRINT_PREF "llx\n", RWORD(pef_hdr_read->entry_point));
     if (RWORD(pef_hdr_read->flags) & PEF_COMPRESSED)
-        fprintf(stderr, TAB "compressed image length: %" PRINT_PREF "d (%.2f%%)\n", RWORD(pef_hdr_read->deflated_image_length),
+        fprintf(stderr, TAB "compressed image length: %" PRINT_PREF "lld (%.2f%%)\n", RWORD(pef_hdr_read->deflated_image_length),
                 100.0 * (float)RWORD(pef_hdr_read->deflated_image_length) / (float)RWORD(pef_hdr_read->image_length));
 }
 
@@ -455,11 +464,11 @@ static void print_segments(void) {
     fprintf(stderr, "Segment table: %d segments\n", RWORD(pef_hdr_read->num_of_segments));
     for (e = 0; e < RWORD(pef_hdr_read->num_of_segments); e++) {
         fprintf(stderr, TAB "segment %d\n", e);
-        fprintf(stderr, TAB TAB "physical address: 0x%x\n", RWORD(pef_segment_table[e].phys_addr));
-        fprintf(stderr, TAB TAB "virtual address: 0x%" PRINT_PREF "x\n", RWORD(pef_segment_table[e].virt_addr));
-        fprintf(stderr, TAB TAB "file size: %" PRINT_PREF "d\n", RWORD(pef_segment_table[e].file_size));
+        fprintf(stderr, TAB TAB "physical address: 0x%llx\n", RWORD(pef_segment_table[e].phys_addr));
+        fprintf(stderr, TAB TAB "virtual address: 0x%" PRINT_PREF "llx\n", RWORD(pef_segment_table[e].virt_addr));
+        fprintf(stderr, TAB TAB "file size: %" PRINT_PREF "lld\n", RWORD(pef_segment_table[e].file_size));
         if (RWORD(pef_hdr_read->flags) & PEF_COMPRESSED)
-            fprintf(stderr, TAB TAB "compressed file size: %" PRINT_PREF "d (%.2f%%)\n", RWORD(pef_segment_table[e].deflated_file_size),
+            fprintf(stderr, TAB TAB "compressed file size: %" PRINT_PREF "lld (%.2f%%)\n", RWORD(pef_segment_table[e].deflated_file_size),
                     100.0 * (float)RWORD(pef_segment_table[e].deflated_file_size) / (float)RWORD(pef_segment_table[e].file_size));
     }
 }
@@ -469,11 +478,11 @@ static void print_custom_files(void) {
     fprintf(stderr, "custom_file table: %d custom_files\n", RWORD(pef_hdr_read->num_of_custom_files));
     for (e = 0; e < RWORD(pef_hdr_read->num_of_custom_files); e++) {
         fprintf(stderr, TAB "custom_file %d\n", e);
-        fprintf(stderr, TAB TAB "address: 0x%" PRINT_PREF "x\n", RWORD(pef_custom_file_table[e].start_addr));
+        fprintf(stderr, TAB TAB "address: 0x%" PRINT_PREF "llx\n", RWORD(pef_custom_file_table[e].start_addr));
         if (!RWORD(pef_custom_file_table[e].size))
             fprintf(stderr, TAB TAB "undefined file size\n");
         else
-            fprintf(stderr, TAB TAB "file size: %" PRINT_PREF "d\n", RWORD(pef_custom_file_table[e].size));
+            fprintf(stderr, TAB TAB "file size: %" PRINT_PREF "lld\n", RWORD(pef_custom_file_table[e].size));
     }
 }
 
