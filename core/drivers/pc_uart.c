51999ff7f41ead5fc1c4a6dd5ac201fe45e88360
prtos-hypervisor <133008648+prtos-hypervisor@users.noreply.github.com>
2026-01-01 23:41:21 +0800

    [ARMv8][QEMU] Make `core/drivers` support armv8 (#32)
    Signed-off-by: 18939117 <18939117@qq.com>

--- core/drivers/pc_uart.c
+++ core/drivers/pc_uart.c
@@ -8,7 +8,7 @@
 
 #include <kdevice.h>
 #include <ktimer.h>
-#if defined(CONFIG_x86) && defined(CONFIG_DEV_UART)
+#if defined(CONFIG_x86) || defined(CONFIG_DEV_UART)
 #include <irqs.h>
 #include <arch/io.h>
 #include <drivers/pc_uart.h>
@@ -16,8 +16,14 @@
 RESERVE_HWIRQ(UART_IRQ0);
 RESERVE_IOPORTS(DEFAULT_PORT, 5);
 
+#if defined(CONFIG_AARCH64)
+#include <arch/layout.h>
+#include <drivers/pl011.h>
+#endif
+
 #define _UART_MAX_FREQ 115200
 static void __init_uart(prtos_u32_t baudrate) {
+#if defined(CONFIG_x86)
     prtos_u16_t div;
     if (!baudrate) return;
     if (baudrate >= _UART_MAX_FREQ)
@@ -32,11 +38,21 @@ static void __init_uart(prtos_u32_t baudrate) {
     out_byte(0x03, DEFAULT_PORT + 3);  // 8 bits, no parity, one stop bit
     out_byte(0xC7, DEFAULT_PORT + 2);  // Enable FIFO, clear them, with 14-byte threshold
     out_byte(0x0B, DEFAULT_PORT + 4);  // IRQs enabled, RTS/DSR set
+#endif
+
+#if defined(CONFIG_AARCH64)
+    pl011_init();
+#endif
 }
 
 static inline void put_char_uart(prtos_s32_t c) {
+#if defined(CONFIG_x86)
     while (!(in_byte(DEFAULT_PORT + 5) & 0x20)) continue;
     out_byte(c, DEFAULT_PORT);
+#endif
+#if defined(CONFIG_AARCH64)
+    pl011_putc(c);
+#endif
 }
 
 static prtos_s32_t write_uart(const kdevice_t *kdev, prtos_u8_t *buffer, prtos_s32_t len) {
